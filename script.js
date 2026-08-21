8da053fbecbee68ac8f010a910ed2bf24e0308ef
Zawa-Boob <mochhafidzzawawi@gmail.com>
2026-08-21 15:15:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,38 @@ const songs = [
 const $ = (id) => document.querySelector(id);
 
 const title = $("#title");
+const artist = $("#artist");
+const link = $("#link");
+const bar = $("#progress");
+const now = $("#now");
+const left = $("#left");
+const status = $("#status");
+const vinyl = $("#vinyl");
+const list = $("#list");
+const playBtn = $("#play");
 
+const audio = new Audio ();
+let currentSongIndex = 0;
+let isPlaying = false;
+
+const formatTime = (seconds) => {
+  if (isNaN(seconds)) return "0:00";
+  const minutes = Math.floor(seconds / 60);
+  const secs = String(Math.floor(seconds % 60)).padStart(2, "o";
+  return `${minutes}:${secs}`;
+};
+
+function updateUI() {
+  const [name, singer, duration, audioFile] = songs[currentSongindex;
+
+title.textContent = name;
+artist.textContent = singer;
+
+link.Atrribute("href");
+link.style.cursor = "default";
+
+list.innerHTML = songs.map((songData, index => {
+  const isActive = index === currentSongIndex ? "active" : "";
+  return `
+    <a class="song-item ${isActive}" data-song="${index}" href=javascript:void(0);">
+    <span>
