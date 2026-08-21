c8bdd110b353f1ef675dc77b239ce1132e82089b
Zawa-Boob <mochhafidzzawawi@gmail.com>
2026-08-21 11:31:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-ffg
+const songs = [
+  ["Golden Hour", "JVKE", 209, "Golden Hour.mp3"],
+  ["Ceilings", "Lizzy McAlpine", 194, "Ceilings.mp3"],
+  ["Death Bed", "Powfu ft. beabadoobee", 173, "Dead Bed.mp3"],
